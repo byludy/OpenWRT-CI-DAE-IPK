5db372d727d578c66c85ec1e6ff27159b5aef8dd
byludy <56289598+byludy@users.noreply.github.com>
2026-04-26 13:04:26 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -79,25 +79,6 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
-
-# 只有ipk可以用
-UPDATE_PACKAGE "luci-app-store" "linkease/istore" "main"
-
-# 我新加的
-#UPDATE_PACKAGE "luci-app-netwizard" "sirpdboy/luci-app-netwizard" "main"
-#echo "CONFIG_PACKAGE_luci-app-netwizard=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
-
-UPDATE_PACKAGE "luci-app-watchdog" "sirpdboy/luci-app-watchdog" "main"
-echo "CONFIG_PACKAGE_luci-app-watchdog=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
-
-UPDATE_PACKAGE "luci-app-rtp2httpd" "stackia/rtp2httpd" "main"
-echo "CONFIG_PACKAGE_luci-app-rtp2httpd=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
-#mv luci-app-rtp2httpd/openwrt-support/* luci-app-rtp2httpd/
-
-# 迅雷有问题
-#UPDATE_PACKAGE "luci-app-thunder" "byludy/luci-app-thunder" "main"
-#echo "CONFIG_PACKAGE_luci-app-thunder=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
-
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
@@ -152,6 +133,7 @@ cp -r $GITHUB_WORKSPACE/package/* ./
 #修复daed/Makefile
 #rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
 sed -i 's/pnpm install ; \\/pnpm install --no-frozen-lockfile ; \\/g' luci-app-daed/daed/Makefile
+sed -i 's|/run/i\\  procd_set_param|/procd_set_param command/i \\\tprocd_set_param|g' luci-app-daed/luci-app-daed/root/etc/init.d/luci_daed
 #cat luci-app-daed/daed/Makefile
 #修复libubox报错
 #sed -i '/include $(INCLUDE_DIR)\/cmake.mk/a PKG_BUILD_FLAGS:=no-werror' ../package/libs/libubox/Makefile
